949f350164e490683b3b85a1d351327dd2a65ece
Elisha Reign Mier <elishamier13@gmail.com>
2025-03-21 22:36:18 +0800

    up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <h1 className="title">Welcome Back</h1>
+      
+      {/* Email Input */}
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Email"
+          className="custom-input"
+        />
+      </div>
+
+      {/* Password Input */}
+      <div className="input-container">
+        <input
+          type="password"
+          placeholder="Password"
+          className="custom-input"
+        />
+      </div>
+
+      {/* Login Button */}
+      <button className="login-btn">Login</button>
+
+      {/* Sign Up Link */}
+      <p className="signup-text">
+        Don't have an account? <Link to="/signup" className="signup-link">Sign Up</Link>
+      </p>
     </div>
   );
 }
---
+++ frontend/src/components/SignUp.jsx
@@ -0,0 +1,55 @@
+import './SignUp.css';
+
+function SignUp() {
+  return (
+    <div className="SignUp">
+      <h1 className="title">Create an Account</h1>
+
+      {/* Name Input */}
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Username"
+          className="custom-input"
+        />
+      </div>
+
+      {/* Email Input */}
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Email"
+          className="custom-input"
+        />
+      </div>
+
+      {/* Password Input */}
+      <div className="input-container">
+        <input
+          type="password"
+          placeholder="Password"
+          className="custom-input"
+        />
+      </div>
+
+      {/* Confirm Password Input */}
+      <div className="input-container">
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          className="custom-input"
+        />
+      </div>
+
+      {/* Sign Up Button */}
+      <button className="login-btn">Sign Up</button>
+
+      {/* Back to Login Link */}
+      <p className="signup-text">
+        Already have an account? <a href="/" className="signup-link">Login</a>
+      </p>
+    </div>
+  );
+}
+
+export default SignUp;
